d11a6c1cc47f83b4c46e952cbcb4211df5887f0f
konnic <konstantin.nicolaus@gmail.com>
2017-05-15 08:31:23 +0800

    info popup, first filter dropdown tries, main font

---
+++ public/scripts/scripts.js
@@ -0,0 +1,8 @@
+// //Function to show Popup
+// function dropShow() {
+//   document.getElementById('testDrop').style.display = "list-item";
+// }
+// //Function to Hide Popup
+// function dropHide(){
+//   document.getElementById('testDrop').style.display = "none";
+// }
